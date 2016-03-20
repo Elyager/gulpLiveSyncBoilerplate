7baba48d7363c5474ee24826ff17a8277567bc40
Elyager <elyager@gmail.com>
2016-03-19 19:02:31 -0700

    Add gulp-plumber and gulp-util  to avoid errors to stop the watching console

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@ var gulp          = require('gulp')
 var browserSync   = require('browser-sync')
 var sass          = require('gulp-sass')
 var jade          = require('gulp-jade')
+var plumber       = require('gulp-plumber')
+var gutil         = require('gulp-util')
+var watching      = true
 
 gulp.task('sass', function() {
   gulp.src('src/scss/*.scss')
+    .pipe(watching ? plumber() : gutil.noop())
     .pipe(sass())
     .pipe(gulp.dest('dist/css'))
 })
@@ -13,6 +17,7 @@ gulp.task('jade', function() {
   var YOUR_LOCALS = {}
 
   gulp.src('src/views/*.jade')
+    .pipe(watching ? plumber() : gutil.noop())
     .pipe(jade({
       locals: YOUR_LOCALS,
       pretty:true
@@ -22,6 +27,7 @@ gulp.task('jade', function() {
 
 gulp.task('copy-js-files', function(){
     gulp.src('src/js/*.js')
+    .pipe(watching ? plumber() : gutil.noop())
     .pipe(gulp.dest('dist/js'))
 })
 
