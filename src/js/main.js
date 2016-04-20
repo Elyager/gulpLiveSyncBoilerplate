1463ba96ec7024803941b99ebbc5e77c47574cce
Elyager <elyager@gmail.com>
2016-04-19 23:38:12 -0700

    - remove unnecessary code

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 //we are loading the JS file at the end of the body,
 // but you know paranoia
-window.onload = function (){
-  console.log('The DOM is loaded!');
-};
+// window.onload = function (){
+//   console.log('The DOM is loaded!');
+// };
 
 // Another way to do it
 // document.addEventListener('DOMContentLoaded', function(){
@@ -11,7 +11,3 @@ window.onload = function (){
 $(document).ready(function() {
   console.log('Jquery is wokring!');
 });
-
-function prueba() {
-  console.log('prueba');
-}
